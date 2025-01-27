a2810b390a9bcffc33699c4efde8f46bae5bc9d3
Taylor <taylormo@unc.edu>
2025-01-27 04:09:12 +0000

    my first rust program

---
+++ hello/target/debug/deps/hello-96d1e18de555afe4.d
@@ -0,0 +1,5 @@
+/workspaces/rust-project/hello/target/debug/deps/hello-96d1e18de555afe4: src/main.rs
+
+/workspaces/rust-project/hello/target/debug/deps/hello-96d1e18de555afe4.d: src/main.rs
+
+src/main.rs:
